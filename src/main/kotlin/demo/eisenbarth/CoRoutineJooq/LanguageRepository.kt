658e22d272657a18486a773f316eb8842d7c6a19
oliver.eisenbarth <oliver.eisenbarth@gmail.com>
2023-10-02 18:00:54 +0200

    Add XML support
    The LanguageRepository has been refactored from an interface to an open class to provide more data fetching options, including JSON and XML.

--- src/main/kotlin/demo/eisenbarth/CoRoutineJooq/LanguageRepository.kt
+++ src/main/kotlin/demo/eisenbarth/CoRoutineJooq/LanguageRepository.kt
@@ -6,35 +6,42 @@ import kotlinx.coroutines.reactive.awaitFirstOrNull
 import org.jooq.DSLContext
 import org.jooq.JSON
 import org.jooq.Record1
-import org.jooq.impl.DSL.jsonEntry
-import org.jooq.impl.DSL.jsonObject
+import org.jooq.XML
+import org.jooq.impl.DSL.*
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 
 @Repository
 @Transactional(readOnly = true)
-interface LanguageRepository {
-    suspend fun findById(id: Int): Language?
-    suspend fun findByIdJson(id: Int): Record1<JSON>?
-    // TODO
-    //  - Showcase multisets
-}
+open class LanguageRepository(@Qualifier("r2dbcContext") private val ctx: DSLContext){
 
-@Repository
-class LanguageRepositoryImpl(@Qualifier("r2dbcContext") private val ctx: DSLContext) : LanguageRepository {
-    override suspend fun findById(id: Int): Language? = ctx
+    suspend fun findByIdAsDTO(id: Int): Language? = ctx
         .selectFrom(LANGUAGE)
         .where(LANGUAGE.LANGUAGE_ID.eq(id))
         .awaitFirstOrNull()
         ?.into(Language::class.java)
 
-    override suspend fun findByIdJson(id: Int): Record1<JSON>? = ctx
+    // If you do not want to take heavy dependencies like Jackson,
+    // JOOQ can map Results to JSON or XML in any shape.
+    suspend fun findById(id: Int): Record1<JSON>? = ctx
         .select(
             jsonObject(
                 jsonEntry("languageId", LANGUAGE.LANGUAGE_ID),
                 jsonEntry("name", LANGUAGE.NAME)
             ).`as`("jsonResult")
+        )
+        .from(LANGUAGE)
+        .where(LANGUAGE.LANGUAGE_ID.eq(id))
+        .awaitFirstOrNull()
+
+    suspend fun findByIdAsXML(id: Int): Record1<XML>? = ctx
+        .select(
+            xmlelement(name("language"),
+                xmlattributes(),
+                xmlelement(name("languageId"), LANGUAGE.LANGUAGE_ID),
+                xmlelement(name("name"), LANGUAGE.NAME)
+            ).`as`("xmlResult")
         )
         .from(LANGUAGE)
         .where(LANGUAGE.LANGUAGE_ID.eq(id))
