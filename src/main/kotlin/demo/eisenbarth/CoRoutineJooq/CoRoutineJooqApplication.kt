bd9c384b2f9083942f1f8b67199b58721923e120
oliver.eisenbarth <oliver.eisenbarth@gmail.com>
2023-10-03 10:31:16 +0200

    Imports

--- src/main/kotlin/demo/eisenbarth/CoRoutineJooq/CoRoutineJooqApplication.kt
+++ src/main/kotlin/demo/eisenbarth/CoRoutineJooq/CoRoutineJooqApplication.kt
@@ -1,15 +1,7 @@
 package demo.eisenbarth.CoRoutineJooq
 
-import org.jooq.DSLContext
-import org.jooq.SQLDialect
-import org.jooq.impl.DSL
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
-import org.springframework.jdbc.datasource.DriverManagerDataSource
 
 @SpringBootApplication
 class CoRoutineJooqApplication {}
